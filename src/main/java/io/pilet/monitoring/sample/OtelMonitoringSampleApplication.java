b7e7fbd4401da31581a4e196919fb85884520478
Alex Potapov <potapov@yahoo.com>
2024-07-17 11:32:13 -0400

    first commit

---
+++ src/main/java/io/pilet/monitoring/sample/OtelMonitoringSampleApplication.java
@@ -0,0 +1,13 @@
+package io.pilet.monitoring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelMonitoringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtelMonitoringSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pilet/monitoring/sample/OtelMonitoringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pilet.monitoring.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelMonitoringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
