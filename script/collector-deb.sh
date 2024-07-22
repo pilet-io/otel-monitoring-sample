e40749845a38db77789cff3c907d05a248f9b9f4
Alex Potapov <potapov@yahoo.com>
2024-07-22 08:13:40 -0400

    initial

--- script/collector-deb.sh
+++ script/collector-deb.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update
 sudo apt-get -y install wget systemctl
-wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.105.0/otelcol-contrib_0.105.0_linux_386.deb
-sudo dpkg -i otelcol-contrib_0.105.0_linux_386.deb
+wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.105.0/otelcol-contrib_0.105.0_linux_amd64.deb
+sudo dpkg -i otelcol-contrib_0.105.0_linux_amd64.deb
