6b8c0f68559222ee1d699acdca2eae1cae2193f6
apotapov <apotapov@westernasset.com>
2024-11-21 11:03:50 -0500

    COMPLIANCE-13434

--- python/run.sh
+++ python/run.sh
@@ -2,7 +2,7 @@
 
 
 export OTEL_SERVICE_NAME="otel"
-export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="http://my-api-endpoint/v1/traces"
+export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="http://localhost:4318/v1/traces"
 export OTEL_EXPORTER_OTLP_PROTOCOL="http/protobuf"
 
 opentelemetry-instrument --traces_exporter console,otlp uvicorn app:app &
