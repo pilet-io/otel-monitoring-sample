a816ae7ec14f58960e893375fc1222341cf3c617
root <root@otel.lxd>
2024-12-03 14:19:50 +0000

    initital

--- python/main.py
+++ python/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from openai import OpenAI
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+OpenAIInstrumentor().instrument()
 
 app = FastAPI()
+client = OpenAI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ])
+    return {"message": completion.choices[0].message}
--- python/run.sh
+++ python/run.sh
@@ -2,7 +2,8 @@
 
 
 export OTEL_SERVICE_NAME="otel"
+export OTEL_TRACES_EXPORTER="otlp"
 export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="http://127.0.0.1:4318/v1/traces"
 export OTEL_EXPORTER_OTLP_PROTOCOL="http/protobuf"
 
-opentelemetry-instrument --traces_exporter console,otlp uvicorn app:app &
+opentelemetry-instrument uvicorn main:app &
